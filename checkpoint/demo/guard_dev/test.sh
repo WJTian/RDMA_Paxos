499e845adfbf9e38f9aba56bc4e0c1dff0c2cb83
jingleyang <yjyfrombnu@gmail.com>
2016-04-15 17:07:09 +0800

    guard dev

--- checkpoint/demo/guard_dev/test.sh
+++ checkpoint/demo/guard_dev/test.sh
@@ -1,37 +1,39 @@
 #!/bin/sh
 
 # before dump
-REDIS_CLI="./apps/redis-cli -h 10.22.1.1 -p 26379"
+REDIS_CLI_1="./apps/redis-cli -h 10.22.1.1 -p 26379"
+REDIS_CLI_2="./apps/redis-cli -h 10.22.1.2 -p 26379"
+REDIS_CLI_3="./apps/redis-cli -h 10.22.1.3 -p 26379"
 
-SERVICE_IP="10.22.1.1"
 CK_NODE="1"  # 10.22.1.2
 RE_NODE="2"  # 10.22.1.3
 
 ps -elf | grep redis
-echo "set key 1" | $REDIS_CLI 
-echo "get key" | $REDIS_CLI
+echo "set name redis_1" | $REDIS_CLI_1
+echo "set key 1" | $REDIS_CLI_1
+echo "set name redis_2" | $REDIS_CLI_2
+echo "set key 1" | $REDIS_CLI_2
+echo "set name redis_3" | $REDIS_CLI_3
+echo "set key 1" | $REDIS_CLI_3
 
-#curl "http://$SERVICE_IP:12345/checkpoint?node_id=$CK_NODE&round_id=1"
+echo "get name" | $REDIS_CLI_1
+echo "get name" | $REDIS_CLI_2
+echo "get name" | $REDIS_CLI_3
+
 echo "checkpoint $CK_NODE 1\n" | nc -U /tmp/guard.sock
 sleep 3 
-#curl "http://$SERVICE_IP:12345/checkpoint?node_id=$CK_NODE&round_id=2"
 echo "checkpoint $CK_NODE 2\n" | nc -U /tmp/guard.sock
 sleep 3 
 echo "checkpoint finished"
 
-echo "set key 2" | $REDIS_CLI 
-echo "set key 3" | $REDIS_CLI 
-echo "set key 4" | $REDIS_CLI 
-echo "get key" | $REDIS_CLI
-ps -elf | grep redis
+echo "set key 2" | $REDIS_CLI_3 
+echo "set key 3" | $REDIS_CLI_3
+echo "set key 4" | $REDIS_CLI_3
+echo "get key" | $REDIS_CLI_3
 
 sleep 3
-#curl "http://$SERVICE_IP:12345/restore?node_id=$RE_NODE&round_id=5"
 echo "restore $RE_NODE 5\n" | nc -U /tmp/guard.sock
-sleep 3 
-ps -elf | grep redis
 sleep 3
 echo "restore finished"
-echo "get key" | $REDIS_CLI
-
-echo "get key" | ./apps/redis-cli -h 10.22.1.3 -p 26379
+echo "get name" | $REDIS_CLI_3
+echo "get key" | $REDIS_CLI_3 

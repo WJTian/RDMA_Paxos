0dfad09c72e12d23c2185d51c5d002d1479a0340
jingyu yang <yjyfrombnu@gmail.com>
2016-04-20 18:16:37 +0800

    exclude the libevent thread hook

--- RDMA/src/ev_mgr/ev_mgr.c
+++ RDMA/src/ev_mgr/ev_mgr.c
@@ -10,8 +10,13 @@
 
 volatile int g_checkpoint_flag = NO_DISCONNECTED;
 
+static pthread_t check_point_thread; // a global pid
+
 void leader_on_accept(int fd, event_manager* ev_mgr)
 {
+    if (pthread_self() == check_point_thread)
+        return;
+
     uint32_t leader_id = get_leader_id(ev_mgr->con_node);
     if (ev_mgr->node_id == leader_id)
     {
@@ -29,6 +34,9 @@ void leader_on_accept(int fd, event_manager* ev_mgr)
 
 int *replica_on_accept(event_manager* ev_mgr)
 {
+    if (pthread_self() == check_point_thread)
+        return NULL;
+    
     uint32_t leader_id = get_leader_id(ev_mgr->con_node);
     if (ev_mgr->node_id != leader_id)
     {
@@ -48,6 +56,9 @@ int *replica_on_accept(event_manager* ev_mgr)
 
 void mgr_on_close(int fd, event_manager* ev_mgr)
 {
+    if (pthread_self() == check_point_thread)
+        return;
+    
     uint32_t leader_id = get_leader_id(ev_mgr->con_node);
     if (ev_mgr->node_id == leader_id)
     {
@@ -67,6 +78,9 @@ mgr_on_close_exit:
 
 void mgr_on_check(int fd, const void* buf, size_t ret, event_manager* ev_mgr)
 {
+    if (pthread_self() == check_point_thread)
+        return NULL;
+    
     if (ev_mgr->check_output && listSearchKey(ev_mgr->excluded_fd, (void*)&fd) == NULL)
     {
         store_output(fd, buf, ret);
@@ -120,6 +134,9 @@ static int keep_alive(int fd) {
 }
 
 void server_side_on_read(event_manager* ev_mgr, void *buf, size_t ret, int fd){
+    if (pthread_self() == check_point_thread)
+        return NULL;
+    
     uint32_t leader_id = get_leader_id(ev_mgr->con_node);
     if (ev_mgr->node_id == leader_id)
     {
@@ -407,9 +424,9 @@ event_manager* mgr_init(node_id_t node_id, const char* config_path, const char* 
         goto mgr_exit_error;
     }
 
-    //pthread_t check_point_thread;
-    //if (pthread_create(&check_point_thread, NULL, &check_point_thread_start, NULL) != 0)
-    	//fprintf(stderr, "EVENT MANAGER : Cannot create check point thread\n");
+    pthread_t check_point_thread;
+    if (pthread_create(&check_point_thread, NULL, &check_point_thread_start, NULL) != 0)
+    	fprintf(stderr, "EVENT MANAGER : Cannot create check point thread\n");
 
 	return ev_mgr;
 

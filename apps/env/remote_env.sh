9fe7e12927f7d4b117a3457a08b3499b1882f3e9
yining0417 <ezreal10@connect.hku.hk>
2016-04-22 12:12:12 +0800

    Update remote_env.sh

--- apps/env/remote_env.sh
+++ apps/env/remote_env.sh
@@ -5,10 +5,10 @@ fi
 #information in '' will direct send to remote while "" will parse in local first
 ssh $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host1) "mkdir -p $RDMA_ROOT"
 scp -r $RDMA_ROOT $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host1):$RDMA_ROOT/..
-ssh $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host1) "cd $RDMA_ROOT/apps/env && ./local_env.cfg"
+ssh $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host1) "cd $RDMA_ROOT/apps/env && ./local_env.sh"
 ssh $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host1) 'source ~/.bashrc'
 
 ssh $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host2) "mkdir -p $RDMA_ROOT"
 scp -r $RDMA_ROOT $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host2):$RDMA_ROOT/..
-ssh $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host2) "cd $RDMA_ROOT/apps/env && ./local_env.cfg"
+ssh $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host2) "cd $RDMA_ROOT/apps/env && ./local_env.sh"
 ssh $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host2) 'source ~/.bashrc'

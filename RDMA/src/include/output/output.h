b23df142469235a0a3e0968a3b86ea72c5a6c0a1
jingyu yang <yjyfrombnu@gmail.com>
2016-04-24 17:08:06 +0800

    change hash buffer size to 1024

--- RDMA/src/include/output/output.h
+++ RDMA/src/include/output/output.h
@@ -12,7 +12,7 @@ Description: Output hash value implementation.
 
 #define CHECK_PERIOD 10
 #define CHECK_GOBACK 5
-#define HASH_BUFFER_SIZE 16
+#define HASH_BUFFER_SIZE 1024
 // since max tcp port number will not exceed this number
 #define MAX_FD_SIZE 65536 
 

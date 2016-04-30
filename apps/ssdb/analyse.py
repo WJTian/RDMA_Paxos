8cc3b6bf178d1d55306fa9439a74b5eb2a67c277
jingleyang <yjyfrombnu@gmail.com>
2016-04-30 20:54:04 +0800

    analyse.py example

--- apps/ssdb/analyse.py
+++ apps/ssdb/analyse.py
@@ -2,13 +2,16 @@
 
 import sys
 
+qps_sum=0
+qps_cnt=0
 for line in sys.stdin:
     if not line:
         break
     if line.startswith("==="):
-        print line
+        #print line
+	pass
     if line.startswith("qps"):
-        print line
+        #print line
         parts = line.split(",")
         if len(parts) !=2:
             break
@@ -18,6 +21,12 @@ for line in sys.stdin:
         t = p[1]
         qps = float(qps)
         t = float(t)
-        ans0 = 1000.0*1000.0/qps
-        ans1 = t*1000.0*1000.0/10000.0 # we sent 10000 requests
-        print "one request time(us): ",ans0," ",ans1
+        #ans0 = 1000.0*1000.0/qps
+        #ans1 = t*1000.0*1000.0/10000.0 # we sent 10000 requests
+        #print "one request time(us): ",ans0," ",ans1
+	qps_cnt+=1
+	qps_sum+=qps
+
+qps_avg= qps_sum/qps_cnt
+print "throughput: ",qps_avg
+print "latency: ","Not supported yet"

6515aa451def6b803bc339db01a815e1a1361bde
jingyu yang <jingleyang@users.noreply.github.com>
2016-05-01 20:32:55 +0800

    remove node all
    remove node all

--- eval/db_delete.sh
+++ eval/db_delete.sh
@@ -7,3 +7,7 @@ fi
 ssh -f $LOGNAME@$(cat $RDMA_ROOT/apps/env/local_host) "sudo rm -rf ~/.db"
 ssh -f $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host1) "sudo rm -rf ~/.db"
 ssh -f $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host2) "sudo rm -rf ~/.db"
+
+ssh -f $LOGNAME@$(cat $RDMA_ROOT/apps/env/local_host) "sudo rm -rf ~/node*"
+ssh -f $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host1) "sudo rm -rf ~/node*"
+ssh -f $LOGNAME@$(cat $RDMA_ROOT/apps/env/remote_host2) "sudo rm -rf ~/node*"

7add0c57abfa811826d3f36957051bdd7760dd95
wangcheng <WangCheng@wangchengdeMacBook-Air.local>
2016-05-04 20:51:28 +0800

    nano sleep to wait for hash reply

--- RDMA/src/ev_mgr/ev_mgr.c
+++ RDMA/src/ev_mgr/ev_mgr.c
@@ -142,12 +142,12 @@ mgr_on_close_exit:
 // please remember free it after use.
 output_peer_t* prepare_peer_array(int fd, dare_log_entry_t *log_entry_ptr, uint32_t leader_id, long hash_index, int group_size){
     
-    /* because rsm_op() returns when it reaches quorum
+    // because rsm_op() returns when it reaches quorum
     
     struct timespec wait_for_reply;
     wait_for_reply.tv_sec = 0;
     wait_for_reply.tv_nsec = 1000 * 5;
-    nanosleep(&wait_for_reply, NULL); */
+    nanosleep(&wait_for_reply, NULL);
     
     output_peer_t* peer_array = (output_peer_t*)malloc(group_size*sizeof(output_peer_t));
     for (int i=0;i<group_size;i++){
